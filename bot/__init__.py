9a3da8f7e19e113d62adc97f07800c0ee815c0ec
GhoulsBotXd2 <whiteblue075@protonmail.com>
2023-05-02 01:06:12 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,11 +3,11 @@ import asyncio
 from pyrogram import Client
 from dotenv import load_dotenv
 
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
+api_id = int(os.environ.get("26019444"))
+api_hash = os.environ.get("a308fac723905cdbd836414b18f3b3d6")
+bot_token = os.environ.get("6030548386:AAGv8q8mr1xbcS9a2dJQjc1GBlRtrst68IA")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+sudo_users = list(set(int(x) for x in os.environ.get("5860097723 5925926828 6046532356").split()))
 ffmpeg = os.environ.get("FFMPEG", "")
 suffix = os.environ.get("SUFFIX")
 
